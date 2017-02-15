089f602675a7269d906ce7167f6c0c337ef34215
José Miguel Santana García <josemisantanagarcia@gmail.com>
2017-02-15 13:42:04 +0000

    Añadido el fichero HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,8 @@
+//Muestra por pantalla el mensaje "Hola Mundo"
+//Autor: José Miguel Santana
+
 public class HolaMundo {
   public static void main(Strings[] args) {
     System.out.println("Hola Mundo");
   }
-}		
+}
